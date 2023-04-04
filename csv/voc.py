9a7c0d61f48118c9ee9e8a8a75eee0da8dabac4f
thibault-roux <thibault.roux1@laposte.net>
2023-04-04 11:09:55 +0200

    Words in test not in train

--- csv/voc.py
+++ csv/voc.py
@@ -1,3 +1,4 @@
+import pickle
 
 def voc(csvfile):
     voc = set()
@@ -10,3 +11,19 @@ def voc(csvfile):
 if __name__ == "__main__":
     words = voc(csvfile="mytest.csv") - voc(csvfile="ALL.csv") # words in test set but not in training set
     print(len(words)) # less than 1% of vocabulary of test set
+
+    #print(words)
+    todelete = set()
+    for word in words:
+        for c in word:
+            if c not in "éèêçâàîïôùûabcdefghijklmnopqrstuvwxyz0123456789'":
+                todelete.add(word)
+
+    for word in todelete:
+        words.remove(word)
+
+    print(words)
+    print(len(words))
+
+    with open("words.pkl", "wb") as f:
+        pickle.dump(words, f)
