c61d4e90867445ebd2f9b7d4bacd5c48a57dcabd
thibault-roux <thibault.roux1@laposte.net>
2023-04-04 10:56:29 +0200

    Implement wer on full dataset

--- IWER.py
+++ IWER.py
@@ -10,15 +10,15 @@ def IWER(ref, hyp, words):
     for i, word in enumerate(ref_aligned):
         if word in words and binary_list[i] == 0:
             errors += 1
-    return errors / len(ref.split(" ")) * 100
+    return errors, len(ref.split(" "))
 
-def WER(ref, hyp, words):
+def WER(ref, hyp):
     ref_aligned, hyp_aligned, binary_list = levenstein_alignment(ref.split(" "), hyp.split(" "))
     errors = 0
     for i, word in enumerate(ref_aligned):
         if binary_list[i] == 0:
             errors += 1
-    return errors / len(ref.split(" ")) * 100
+    return errors, len(ref.split(" "))
 
 
 # function that cleans a string reference from uncommon accents and encoding bugs
@@ -38,20 +38,43 @@ def clean_string(string):
     return string
 
 def load_data(filename, clean=True):
-    ref = []
-    hyp = []
+    refs = []
+    hyps = []
     with open("data/" + filename, "r", encoding="utf8") as f:
         for line in f:
             line = line.split("\t")
             if clean:
-                ref.append(clean_string(line[1]))
-                hyp.append(clean_string(line[2]))
+                refs.append(clean_string(line[1]))
+                hyps.append(clean_string(line[2]))
             else:
-                ref.append(line[1])
-                hyp.append(line[2])
+                refs.append(line[1])
+                hyps.append(line[2])
+    return refs, hyps
 
+def wer(refs, hyps):
+    errors_total = 0
+    length_total = 0
+    for ref, hyp in zip(refs, hyps):
+        errors, length = WER(ref, hyp)
+        errors_total += errors
+        length_total += length
+    return errors_total / length_total
+
+def iwer(refs, hyps, words):
+    errors_total = 0
+    length_total = 0
+    for ref, hyp in zip(refs, hyps):
+        errors, length = IWER(ref, hyp, words)
+        errors_total += errors
+        length_total += length
+    return errors_total / length_total
 
 
 if __name__ == "__main__":
     # print(IWER("ceci est un exemple", "ce ceci est une exemple pas", {"<epsilon>", "ce", "ceci", "est", "un", "une", "exemple", "pas"}))
 
+    ref, hyp = load_data("KD_woR.txt")
+    print(WER(ref, hyp))
+
+    ref, hyp = load_data("KD_woR.txt", clean=False)
+    print(WER(ref, hyp))
