e2ac7e7cef3b5813929088aaea4292d5be6837b9
thibault-roux <thibault.roux1@laposte.net>
2023-04-07 12:53:44 +0200

    Fix bugs

--- Tokenizer/lenbpe.py
+++ Tokenizer/lenbpe.py
@@ -3,26 +3,28 @@ import matplotlib.pyplot as plt
 # write a function that read a bpe vocab file and return a dictionary a list of length of tokens
 
 def read_vocab(vocab_file):
-    vocab = {}
+    len_tokens = []
     with open(vocab_file, 'r') as f:
         for line in f:
             line = line.strip()
             if line == '':
                 continue
             token, index = line.split('\t')
-            vocab[token] = index
+            len_tokens.append(len(token))
     plot_len_tokens(len_tokens, vocab_file.split('_')[0])
-    return vocab
 
 # write a function that receives a list of length of tokens and save a plot of the distribution of the length of tokens
 
 def plot_len_tokens(len_tokens, num):
     plt.hist(len_tokens, bins=100)
+    # plt.xlim(0, 100)
+    plt.ylim(0, 250)
     plt.xlabel('Length of tokens')
     plt.ylabel('Frequency')
     plt.title('Distribution of the length of tokens for BPE vocab of size {}'.format(num))
     plt.savefig("hist/" + num + '.png')
+    plt.clf()
 
 if __name__ == '__main__':
-    vocab = read_vocab('1000_bpe.vocab')
-    vocab = read_vocab('750_bpe.vocab')
+    read_vocab('1000_bpe.vocab')
+    read_vocab('750_bpe.vocab')
