48d31834fd4384c620bb9c6ac59e80c99bfcdc8d
thibault-roux <thibault.roux1@laposte.net>
2023-06-02 16:58:09 +0200

    Correct print

--- character_rate.py
+++ character_rate.py
@@ -1,4 +1,6 @@
+import pickle
 from utils.leven import levenstein_alignment
+import progressbar
 
 
 def read(filename):
@@ -13,15 +15,22 @@ def read(filename):
     return data
 
 def character_rate(filename):
+    print("Reading data...")
     data = read(filename)
+    print("Data read")
 
     occ = dict()
     err = dict()
 
+    print("Computing...")
+    bar = progressbar.ProgressBar(max_value=len(data))
+    i = 0
     for id, refhyp in data.items():
+        i += 1
+        bar.update(i)
         ref = refhyp[0]
         hyp = refhyp[1]
-        ref, hyp, binary_list = levenstein_alignment(ref, hyp)
+        ref, hyp, binary_list = levenstein_alignment(ref, hyp) # this call takes some time applied on character
         for r, b in zip(ref, hyp):
             if r not in occ:
                 occ[r] = 0
@@ -30,13 +39,36 @@ def character_rate(filename):
                 if r not in err:
                     err[r] = 0
                 err[r] += 1
+    print("End of computation")
 
     for k, v in occ.items():
-        print(k, v, err[k])
+        try:
+            print(k, v, err[k]) # keyError: '7'
+        except KeyError():
+            print(k, v, 0, "KeyError")
+
+    # store in a pickle file the occ
+    with open("pickle/" + filename + "_occ.pkl", "wb") as file:
+        pickle.dump(occ, file)
+
+
+def check(filename):
+    with open("pickle/" + filename + "_occ.pkl", "rb") as file:
+        occ = pickle.load(file)
+    for k, v in occ.items():
+        print(k, v)
 
 if __name__ == "__main__":
-    filename1 = "KD_woR"
-    filename2 = "KD_wR"
+    # filename1 = "KD_woR"
+    # filename2 = "KD_wR"
 
-    character_rate(filename1)
+    check("SB_w2v_7k")
+
+    exit(-1)
+
+
+    # character_rate(filename1)
+    #character_rate("SB_w2v_7k")
+    character_rate("SB_bpe750")
+    character_rate("SB_bpe1000")
     # long à calculer, vérifier pourquoi ?
