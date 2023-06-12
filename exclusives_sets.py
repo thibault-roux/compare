fd4e615c3f6be5ec6da22a752266106f55a3abe7
thibault-roux <thibault.roux1@laposte.net>
2023-06-12 16:13:22 +0200

    Functional version

--- exclusives_sets.py
+++ exclusives_sets.py
@@ -11,15 +11,22 @@ if __name__ == "__main__":
     data2 = read(filename2)
     data1, data2 = intersect(data1, data2)
 
+    dico_list = dict()
+    dico_list[filename1] = []
+    dico_list[filename2] = []
+
     vocab1 = set()
     vocab2 = set()
     for id, refhyp in data1.items():
         vocab1.update(data1[id][1].split())
+        dico_list[filename1] += data1[id][1].split()
         vocab2.update(data2[id][1].split())
+        dico_list[filename2] += data2[id][1].split()
 
 
-    exclusive_vocab1 = sorted(vocab1 - vocab2)
-    exclusive_vocab2 = sorted(vocab2 - vocab1)
+    exclusive_vocab = dict()
+    exclusive_vocab[filename1] = sorted(vocab1 - vocab2)
+    exclusive_vocab[filename2] = sorted(vocab2 - vocab1)
 
     # print sorted list of vocab1 and vocab2
 
@@ -28,6 +35,9 @@ if __name__ == "__main__":
     #     print(word1, word2)
     #     input()
 
-    with open("results/" + filename1 + "_exclusive_vocab.txt", "w", encoding="utf8") as file:
-        for word in exclusive_vocab1:
-            file.write(word + "\n")
+    print(dico_list[filename1][0])
+
+    for filename in [filename1, filename2]:
+        with open("results/" + filename + "_exclusive_vocab.txt", "w", encoding="utf8") as file:
+            for word in exclusive_vocab[filename]:
+                file.write(word + "," + str(dico_list[filename].count(word)) + "\n")
