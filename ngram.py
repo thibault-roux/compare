1a905d6a5dc903df4a6aabc80f54f5f133b29caf
thibault-roux <thibault.roux1@laposte.net>
2023-06-19 10:08:07 +0200

    Probabilities with words

--- ngram.py
+++ ngram.py
@@ -1,10 +1,12 @@
 from utils.io import read, intersect
 
 
-def train_ngram(corpus, n):
+def train_ngram(corpus, n, words=False):
     probs = dict()
     occ = dict()
     for ligne in corpus:
+        if words == True:
+            ligne = ligne.split(" ")
         for i in range(len(ligne)-2):
             c1 = ligne[i]
             c2 = ligne[i+1]
@@ -22,12 +24,19 @@ def train_ngram(corpus, n):
                     probs[c1][c2] += 1
     return probs, occ
 
-def compute_probability(sentence, probs, n, occ): # probs is a dict of probabilities of a n-gram, n is the length of a ngram
+def compute_probability(sentence, probs, n, occ, words=False): # probs is a dict of probabilities of a n-gram, n is the length of a ngram
+    if words == True:
+        sentence = sentence.split(" ")
     probabilities = [] # list of probabilities of each n-gram
     if len(sentence) < n:
         return 0
     for i in range(len(sentence)-1):
-        if i < len(sentence)-1: # -n+1 ?
+        if i < len(sentence)-n:
+            c1 = ""
+            for j in range(n):
+                c1 += sentence[i+j]
+            c2 = sentence[i+n]
+            
             c1 = sentence[i]
             c2 = sentence[i+1]
             try:
@@ -35,12 +44,15 @@ def compute_probability(sentence, probs, n, occ): # probs is a dict of probabili
             except KeyError:
                 proba = 0
             probabilities.append(proba)
+    """print(sentence)
+    print(probabilities)
+    input()"""
     try:
         return sum(probabilities)/len(probabilities)
     except ZeroDivisionError:
         return 0
 
-def evaluate(filename1, filename2, name1, name2):
+def evaluate(filename1, filename2, name1, name2, words=False):
     data1 = read(filename1)
     data2 = read(filename2)
     data1, data2 = intersect(data1, data2)
@@ -57,10 +69,10 @@ def evaluate(filename1, filename2, name1, name2):
     
     print("Training ngram...")
     n = 1
-    probs, occ = train_ngram(data_train, n)
+    probs, occ = train_ngram(data_train, n, words=words)
     # compteur
-    hyp1_better = 0
-    hyp2_better = 0
+    hyp1_more_probable = 0
+    hyp2_more_probable = 0
     equal = 0
 
     for id, refhyp in data1.items():
@@ -68,35 +80,56 @@ def evaluate(filename1, filename2, name1, name2):
             raise Exception("ids are not the same. Check first column of data.")
         if refhyp[0] != data2[id][0]:
             raise Exception("ids are not the same. Check first column of data.")
+        if refhyp[1] == data2[id][1]:
+            continue
 
         # compute probability
         sentence = refhyp[1]
-        probability1 = compute_probability(sentence, probs, n, occ)
+        probability1 = compute_probability(sentence, probs, n, occ, words=words)
         sentence = data2[id][1]
-        probability2 = compute_probability(sentence, probs, n, occ)
-        print(probability1 - probability2)
+        probability2 = compute_probability(sentence, probs, n, occ, words=words)
+        if probability1 < 0 or probability2 < 0 or probability1 > 1 or probability2 > 1:
+            raise Exception("probability is not between 0 and 1")
+        # print(probability1 - probability2)
 
         # update counter
         if probability1 > probability2:
-            hyp1_better += 1
+            hyp1_more_probable += 1
         elif probability1 < probability2:
-            hyp2_better += 1
+            hyp2_more_probable += 1
         else:
             equal += 1
 
-    print(name1, "better: ", hyp1_better)
-    print(name2, "better: ", hyp2_better)
+    print(name1, "more probable: ", hyp1_more_probable)
+    print(name2, "more probable: ", hyp2_more_probable)
     print("equal: ", equal)
 
 
 if __name__ == "__main__":
     # in the future, compute different probabilities with different n-grams values
 
+    words = True # if True, compute probabilities with words, else with characters
+
     filename1 = "SB_bpe1000" # bpe 1000
     name1 = "bpe 1000"
-    filename2 = "SB_bpe750" # char
+    filename2 = "SB_w2v_7k" # char
+    name2 = "char"
+    evaluate(filename1, filename2, name1, name2, words=words)
+    print("---------------------")
+
+    exit(-1)
+
+
+    filename1 = "SB_bpe750" # bpe 750
+    name1 = "bpe 750"
+    filename2 = "SB_w2v_7k" # char
+    name2 = "char"
+    evaluate(filename1, filename2, name1, name2, words=words)
+    print("---------------------")
+
+    filename1 = "SB_bpe1000" # bpe 1000
+    name1 = "bpe 1000"
+    filename2 = "SB_bpe750" # bpe 750
     name2 = "bpe 750"
-
-    evaluate(filename1, filename2, name1, name2)
-
-    
+    evaluate(filename1, filename2, name1, name2, words=words)
+    print("---------------------")
