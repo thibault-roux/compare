d56c8f30c3aabe2cc977b25570c7c4a42214b8ed
thibault-roux <thibault.roux1@laposte.net>
2023-06-19 11:30:51 +0200

    Exclude only references and words from micka doc

--- exclusives_sets.py
+++ exclusives_sets.py
@@ -4,7 +4,7 @@ from utils.io import read, intersect
 
 
 
-if __name__ == "__main__":
+def exclusive_sets():
     filename1 = "SB_bpe1000"
     filename2 = "SB_w2v_7k"
     data1 = read(filename1)
@@ -15,9 +15,12 @@ if __name__ == "__main__":
     dico_list[filename1] = []
     dico_list[filename2] = []
 
+    vocab_reference = set()
+
     vocab1 = set()
     vocab2 = set()
     for id, refhyp in data1.items():
+        vocab_reference.update(data1[id][0].split())
         vocab1.update(data1[id][1].split())
         dico_list[filename1] += data1[id][1].split()
         vocab2.update(data2[id][1].split())
@@ -25,8 +28,8 @@ if __name__ == "__main__":
 
 
     exclusive_vocab = dict()
-    exclusive_vocab[filename1] = sorted(vocab1 - vocab2)
-    exclusive_vocab[filename2] = sorted(vocab2 - vocab1)
+    exclusive_vocab[filename1] = sorted(vocab1 - vocab2 - vocab_reference)
+    exclusive_vocab[filename2] = sorted(vocab2 - vocab1 - vocab_reference)
 
     # print sorted list of vocab1 and vocab2
 
@@ -41,3 +44,34 @@ if __name__ == "__main__":
         with open("results/" + filename + "_exclusive_vocab.txt", "w", encoding="utf8") as file:
             for word in exclusive_vocab[filename]:
                 file.write(word + "," + str(dico_list[filename].count(word)) + "\n")
+
+    # ne pas faire ça, générer juste l'exclusion (sans prendre en compte l'intersection de deux systèmes)
+
+
+
+
+def original_vocab(filename):
+    data = read(filename)
+
+    with open("/local_disk/atlantia/laboinfo/rouvier/lm/vocab/uniq.old", "r", encoding="utf8") as file:
+        vocab_to_delete = set(file.read().split())
+
+    vocab_reference = set()
+    vocab_hypothesis = set()
+    for id, refhyp in data.items():
+        vocab_reference.update(data[id][0].split())
+        vocab_hypothesis.update(data[id][1].split())
+    
+    exclusive_vocab = sorted(vocab_hypothesis - vocab_reference - vocab_to_delete)
+
+    with open("results/" + filename + "_exclusive_reference.txt", "w", encoding="utf8") as file:
+        for word in exclusive_vocab:
+            file.write(word + "\n")
+
+
+if __name__ == "__main__":
+    # exclusive_sets()
+
+    original_vocab("SB_bpe750")
+    original_vocab("SB_bpe1000")
+    original_vocab("SB_w2v_7k")
